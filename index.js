01d7d78ab9942c0e38f475b053d74bd9e040f9a5
shakardukhthaidari <162323415+shakardukhthaidari@users.noreply.github.com>
2024-03-07 20:08:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
 
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
 
-    let weather = {
-        paris: {
-          temp: 19.7,
-          humidity: 80,
-        },
-        tokyo: {
-          temp: 17.3,
-          humidity: 50,
-        },
-        lisbon: {
-          temp: 30.2,
-          humidity: 20,
-        },
-        "san francisco": {
-          temp: 20.9,
-          humidity: 100,
-        },
-        oslo: {
-          temp: -5,
-          humidity: 20,
-        },
-      };
-      
-      window.onload = function cityWeather() {
-        let city = prompt("Enter a city.").toLowerCase();
-        let cityWeather = weather[city];
-        
-        if (cityWeather !== undefined) {
-          alert(
-            `It is currently ${cityWeather.temp}°C in ${city} with a humidity of ${cityWeather.humidity}`
-          );
-        } else {
-          alert(
-            "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
-          );
-        }
-        
-        //let weather = math.ciel(weather.humidity);
-      };
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
